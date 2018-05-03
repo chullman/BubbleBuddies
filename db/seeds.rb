53d98e2ac9e13f1537598bd4a5f6b371508eb242
chullman <christopherhullman@gmail.com>
2018-05-03 10:57:32 +1000

    cloudinary initial

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Role.create([{ role: 'admin' }, { role: 'normal' }])
+
+Cloudinary::Uploader.upload(Rails.root.join('app', 'assets', 'images', 'admin-diver.jpg'), :public_id => 'admin-diver')
+
+user = User.new
+user.email = 'admin@admin.com'
+user.password = 'admin'
+user.first_name = 'admin'
+user.last_name = 'admin'
+user.role = Role.where(role: 'admin').first
+user.is_disabled = false
+user.image_cl_url = Cloudinary::Utils.cloudinary_url('admin-diver')
+user.save!
+
+Cloudinary::Uploader.upload(Rails.root.join('app', 'assets', 'images', 'test-diver.jpg'), :public_id => 'test-diver')
+
+user = User.new
+user.email = 'test@test.com'
+user.password = 'test'
+user.first_name = 'test'
+user.last_name = 'test'
+user.role = Role.where(role: 'normal').first
+user.is_disabled = false
+user.image_cl_url = Cloudinary::Utils.cloudinary_url('test-diver')
+user.save!
