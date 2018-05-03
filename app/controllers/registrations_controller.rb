04fb6d8e2067cad86ff95fed5c9d76521cbefc29
chullman <christopherhullman@gmail.com>
2018-05-03 12:27:24 +1000

    edit registration complete

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -56,7 +56,7 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation, :current_password, :is_disabled, :image)
+    params.require(:user).permit(:first_name, :last_name, :password, :password_confirmation, :current_password, :is_disabled, :image)
   end
 
 end
