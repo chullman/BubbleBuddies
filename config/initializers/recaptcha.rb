b377431ee65465836ee4670c8ccb3b62318d0ec6
chullman <christopherhullman@gmail.com>
2018-05-04 13:36:42 +1000

    config var fixes

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,9 @@
 Recaptcha.configure do |config|
-  config.site_key = "6LcsJ1cUAAAAACFEL6Iw9X3XHqnl0HHpCHdM5YQm"
-  config.secret_key = "6LcsJ1cUAAAAAKzFgdbc2grwWlsW4QDGgcuuKSPj"
+  if Rails.env.development?
+    config.site_key = "6LcsJ1cUAAAAACFEL6Iw9X3XHqnl0HHpCHdM5YQm"
+    config.secret_key = "6LcsJ1cUAAAAAKzFgdbc2grwWlsW4QDGgcuuKSPj"
+  elsif Rails.env.production?
+    config.site_key = "6LcKLFcUAAAAADk8IzYsGnhYjtifkngbPuObxyyZ"
+    config.secret_key = "6LcKLFcUAAAAAOY5n0Vo-c65qMxfRMwfcTGIw5AV"
+  end
 end
