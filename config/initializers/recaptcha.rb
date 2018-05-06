1b63d670313e0a1420f349c9b798cc2d54f2e4f4
chullman <christopherhullman@gmail.com>
2018-05-06 12:48:35 +1000

    vars relocation fix

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,9 +1,9 @@
 Recaptcha.configure do |config|
   if Rails.env.development?
-    config.site_key = "6LcsJ1cUAAAAACFEL6Iw9X3XHqnl0HHpCHdM5YQm"
-    config.secret_key = "6LcsJ1cUAAAAAKzFgdbc2grwWlsW4QDGgcuuKSPj"
+    config.site_key = ENV['recaptcha_site_key_dev']
+    config.secret_key = ENV['recpatcha_secret_key_dev']
   elsif Rails.env.production?
-    config.site_key = "6LcKLFcUAAAAADk8IzYsGnhYjtifkngbPuObxyyZ"
-    config.secret_key = "6LcKLFcUAAAAAOY5n0Vo-c65qMxfRMwfcTGIw5AV"
+    config.site_key = ENV['recaptcha_site_key_prod']
+    config.secret_key = ENV["recaptcha_secret_key_prod"]
   end
 end
