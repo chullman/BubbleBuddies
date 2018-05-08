be47062b4ee136c72496272be729cf20bc9586a9
chullman <christopherhullman@gmail.com>
2018-05-08 15:07:49 +1000

    fix for heroku deployment

--- config/initializers/rolify.rb
+++ config/initializers/rolify.rb
@@ -7,4 +7,6 @@ Rolify.configure do |config|
   
   # Configuration to remove roles from database once the last resource is removed. Default is: true
   config.remove_role_if_empty = false
+
+  
 end
