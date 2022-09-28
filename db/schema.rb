9d50c32bef6e87f8b4e8dc1d732d86ce9adbb16c
Christopher Hullman <christopherhullman@gmail.com>
2022-09-28 15:05:46 +1000

    schema with devise added

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2022_09_28_004631) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
