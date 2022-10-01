db1a41dbaa95420f981c2c1cd7fc50ee75f4fe57
Christopher Hullman <christopherhullman@gmail.com>
2022-10-01 10:43:18 +1000

    configured basic cloudinary and carrierwave with active storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable
+
+  mount_uploader :image, ImageUploader
+
+  has_one_attached :image
+  
 end
