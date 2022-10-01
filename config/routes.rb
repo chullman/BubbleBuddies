265cf4cfceca58f3fb2e06ced84ce85fca806d36
Christopher Hullman <christopherhullman@gmail.com>
2022-10-01 15:59:28 +1000

    bug fixes around styling and recaptcha

--- config/routes.rb
+++ config/routes.rb
@@ -27,7 +27,7 @@ Rails.application.routes.draw do
 
   get 'home/index'
   devise_for :users, controllers: {
-    sessions: 'users/sessions'
+    registrations: 'users/registrations'
   }
 
   root to: "home#index"
