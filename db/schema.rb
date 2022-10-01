90bd56f33fe12fed7fee26a34ebf61c29090ebda
Christopher Hullman <christopherhullman@gmail.com>
2022-10-01 11:32:45 +1000

    css and static images added

--- db/schema.rb
+++ db/schema.rb
@@ -53,6 +53,12 @@ ActiveRecord::Schema.define(version: 99) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "certagencies", force: :cascade do |t|
+    t.string "cert_agency"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "roles", force: :cascade do |t|
     t.string "name"
     t.string "resource_type"
