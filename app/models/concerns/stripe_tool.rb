0a78a35aaa0d1bf5b05d997a299d6f4cd6352863
Christopher Hullman <christopherhullman@gmail.com>
2022-10-04 18:06:27 +1000

    trying to fix circular argument reference error

--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -1,16 +1,16 @@
 module StripeTool
-  def self.create_customer(email: email_address, stripe_token: stripe_token)
+  def self.create_customer(email: email_address, stripe_token: stripe_token_value)
     Stripe::Customer.create(
       email: email_address,
-      source: stripe_token
+      source: stripe_token_value
     )
   end
 
-  def self.create_charge(customer_id: customer_id, amount: amount, description: description)
+  def self.create_charge(customer_id: customer_id_value, amount: amount_value, description: description_value)
     Stripe::Charge.create(
-      customer: customer_id,
-      amount: amount,
-      description: description,
+      customer: customer_id_value,
+      amount: amount_value,
+      description: description_value,
       currency: 'aud'
     )
   end
