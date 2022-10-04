1435d39038d34650431d3afc0030fcbc7d736c20
Christopher Hullman <christopherhullman@gmail.com>
2022-10-04 17:48:59 +1000

    trying to fix circular argument reference error

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,5 +1,3 @@
-require 'stripe'
-
 Rails.configuration.stripe = {
     :publishable_key => Rails.application.credentials.dig(:STRIPE_PUBLIC_KEY) || ENV['STRIPE_PUBLIC_KEY'],
     :secret_key => Rails.application.credentials.dig(:STRIPE_PRIVATE_KEY) || ENV['STRIPE_PRIVATE_KEY']
