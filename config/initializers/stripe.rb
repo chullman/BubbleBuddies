65ddbaca11c11177246bd59508a2b9809b2e7e91
Christopher Hullman <christopherhullman@gmail.com>
2022-10-04 15:42:22 +1000

    stripe should be correctly configured

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 require 'stripe'
 
 Rails.configuration.stripe = {
-    :publishable_key => Rails.application.credentials.dig(:STRIPE_PUBLIC_KEY) || ENV['STRIPE_PUBLIC_KEY']
-    :secret_key      => Rails.application.credentials.dig(:STRIPE_PRIVATE_KEY) || ENV['STRIPE_PRIVATE_KEY']
+    :publishable_key => Rails.application.credentials.dig(:STRIPE_PUBLIC_KEY) || ENV['STRIPE_PUBLIC_KEY'],
+    :secret_key => Rails.application.credentials.dig(:STRIPE_PRIVATE_KEY) || ENV['STRIPE_PRIVATE_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
