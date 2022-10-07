941a88cc229e020632b0f3a69efaaf54a66f49a2
Christopher Hullman <christopherhullman@gmail.com>
2022-10-07 10:16:43 +1000

    fixed issue with variables in stripe_tool.rb module

--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -1,17 +1,17 @@
 module StripeTool
-  def self.create_customer(email: email_address, stripe_token: stripe_token_value)
+  def self.create_customer(email:, stripe_token:)
     Stripe::Customer.create(
-      email: email_address,
-      source: stripe_token_value
+      email: email,
+      source: stripe_token
     )
   end
 
-  def self.create_charge(customer_id: customer_id_value, amount: amount_value, description: description_value)
+  def self.create_charge(customer_id:, amount:, description:, currency: 'aud')
     Stripe::Charge.create(
-      customer: customer_id_value,
-      amount: amount_value,
-      description: description_value,
-      currency: 'aud'
+      customer: customer_id,
+      amount: amount,
+      description: description,
+      currency: currency
     )
   end
 end
