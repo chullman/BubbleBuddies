b26707c30d576cd2d5db3520d891333d52267029
Christopher Hullman <christopherhullman@gmail.com>
2022-10-12 13:11:24 +1000

    commented out config.hosts temporarily to debug

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 99) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "timescaledb"
 
   create_table "abletoteaches", force: :cascade do |t|
     t.bigint "instructor_id"
